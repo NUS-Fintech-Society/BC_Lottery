b01e9889d3b94cee41cce67741452bde0d266d5d
Colin <colinchanwh@gmail.com>
2021-01-11 20:45:09 +0800

    contracts and migrations

---
+++ src/contracts/Lottery.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Lottery {
+    using SafeMath for uint256;
+    address public manager;
+    address[] public players;
+
+    constructor() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            );
+    }
+
+    function pickWinner() public {
+        uint256 index = random() % players.length;
+        payable(players[index]).transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+}
